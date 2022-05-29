fad068dc019a35da12070b4f496704bc05261d2e
Thorappan-TG <103317425+Thorappan-TG@users.noreply.github.com>
2022-05-29 08:03:39 +0530

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -10,7 +10,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-├👑 𝙾𝚆𝙽𝙴𝚁 : <a href=https://t.me/Ayseditz>SᴇʙᴀSᴛɪN</a>
+├👑 𝙾𝚆𝙽𝙴𝚁 : <a href=https://t.me/Ayseditz>Ayseditz</a>
 ├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
 ├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
 ├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
